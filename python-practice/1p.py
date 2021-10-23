0e609363be8981d8d4a6adaa5e1a5b172f6342de
Raihan Munim <raihan.srizon@gmail.com>
2021-10-23 18:36:27 +0600

    added

---
+++ python-practice/1p.py
@@ -0,0 +1,21 @@
+# press ctrl k+c to comment 
+print("practicing")
+
+# variables
+var1 = "variable"
+var12 = "12"
+del var12
+print(type(var1))
+
+# input to variable
+# var3 = input("Type something: ")
+# print(var3)
+
+# conditional logic
+print(5!=4)
+
+# conditional logic with variables
+Tom_age = 3
+Age_req = 5
+
+print(Tom_age == Age_req)
